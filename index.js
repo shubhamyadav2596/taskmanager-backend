84b299a41acc368f39908d076fd8a9e486e59db5
progammerBoy <sy8692935@gmail.com>
2026-06-28 16:17:10 +0530

    add origins

--- index.js
+++ index.js
@@ -29,12 +29,22 @@ const app = express()
 
 
 
+const allowedOrigins = [
+  "https://task-manager-sigma-nine-54.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:3000",
+]
+
 app.use(
   cors({
-    origin: [
-      "https://task-manager-sigma-nine-54.vercel.app",
-      "http://localhost:5173"
-    ],
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
